c80423896d9c3d4b7c9442841d3a18bebb40c3a5
shubham sharma <shubhamtech004@gmail.com>
2024-06-13 02:04:22 +0530

    adding express validation  midlleware to validate the form data on server sde

--- src/Middleware/validationMiddleware.js
+++ src/Middleware/validationMiddleware.js
@@ -1,26 +1,79 @@
-const validateRequest = (req,res,next)=>{
-    const { name, price, imageUrl } = req.body;
-    let errors = [];
-    if (!name || name.trim() == '') {
-      errors.push('Name is required');
-    }
-    if (!price || parseFloat(price) < 1) {
-      errors.push(
-        'Price must be a positive value'
-      );
-    }
-    try {
-      const validUrl = new URL(imageUrl);
-    } catch (err) {
-      errors.push('URL is invalid');
-    }
+// 1st and easy way 
 
-    if (errors.length > 0) {
+// const validateRequest = (req,res,next)=>{
+//     const { name, price, imageUrl } = req.body;
+//     let errors = [];
+//     if (!name || name.trim() == '') {
+//       errors.push('Name is required');
+//     }
+//     if (!price || parseFloat(price) < 1) {
+//       errors.push(
+//         'Price must be a positive value'
+//       );
+//     }
+//     try {
+//       const validUrl = new URL(imageUrl);
+//     } catch (err) {
+//       errors.push('URL is invalid');
+//     }
+
+//     if (errors.length > 0) {
+//       return res.render('new-product', {
+//         errorMessage: errors[0],
+//       });
+//     }
+//     next();
+// }
+
+// export default validateRequest;
+
+
+
+// 2nd and new way with express
+
+import {
+    body,
+    validationResult,
+  } from 'express-validator';
+  
+  const validateRequest = async (
+    req,
+    res,
+    next
+  ) => {
+    console.log(req.body);
+    // 1. Setup rules for validation.
+    const rules = [
+      body('name')
+        .notEmpty()
+        .withMessage('Name is required'),
+      body('price')
+        .isFloat({ gt: 0 })
+        .withMessage(
+          'Price should be a positive value'
+        ),
+      body('imageUrl')
+        .isURL()
+        .withMessage('Invalid url'),
+    ];
+  
+    // 2. run those rules.
+    await Promise.all(
+      rules.map((rule) => rule.run(req))
+    );
+  
+    // 3. check if there are any errors after running the rules.
+    var validationErrors = validationResult(req);
+    console.log(validationErrors);
+    // 4. if errros, return the error message
+    if (!validationErrors.isEmpty()) {
       return res.render('new-product', {
-        errorMessage: errors[0],
+        errorMessage:
+          validationErrors.array()[0].msg,
       });
     }
     next();
-}
-
-export default validateRequest;
+  };
+  
+  export default validateRequest;
+  
