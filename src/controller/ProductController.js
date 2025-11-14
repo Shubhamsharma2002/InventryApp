f48dae01bccda67ae353b897da929df01ab1fece
shubham sharma <shubhamtech004@gmail.com>
2025-11-14 23:22:50 +0530

    testing the api

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import ProductModel from '../Model/ProductModel.js';
 export default class ProductController{
-
        getproducts(req,res){
               let product = ProductModel.get();
        //        console.log(Product);
